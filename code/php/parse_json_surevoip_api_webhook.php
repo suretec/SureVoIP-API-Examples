b34672ff43773dab0b0ccd3233ba49223ddc5441
Gavin Henry <ghenry@suretecsystems.com>
2012-09-20 19:37:33 +0100

    How to parse POSTed JSON data in PHP

--- code/php/parse_json_surevoip_api_webhook.php
+++ code/php/parse_json_surevoip_api_webhook.php
@@ -1,6 +1,7 @@
 <?php
 
-$res = json_decode($_REQUEST['data'], true);
-echo json_encode($res);
+$call_data = json_decode(file_get_contents("php://input"));
+echo 'SureVoIP ' . $call_data->surevoip;
+echo "\nSureVoIP API " . $call_data->surevoip_api . "\n";
 
 ?>
